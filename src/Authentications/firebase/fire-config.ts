7f9bedef88e549428fd0e6da4215d733e4bf3da0
sivanorm <sivanormsoft@gmail.com>
2023-11-29 16:49:42 +0530

    fire config added

---
+++ src/Authentications/firebase/fire-config.ts
@@ -0,0 +1,9 @@
+export const config = {
+  apiKey: "AIzaSyBMxJ0d-DRCoYP-oNCTxw6aVka68lj56NU",
+  authDomain: "ramanujanacademy.firebaseapp.com",
+  projectId: "ramanujanacademy",
+  storageBucket: "ramanujanacademy.appspot.com",
+  messagingSenderId: "140451379871",
+  appId: "1:140451379871:web:8ce89ab8348c8137be80aa",
+  measurementId: "G-1GTEYQCHCW",
+};
