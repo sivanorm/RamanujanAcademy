7dd1f28283b5945dace0fbe44898beab41de3e65
sivanorm <sivanormsoft@gmail.com>
2023-11-29 16:41:22 +0530

    fire config test

--- src/Authentications/firebase/firebase.ts
+++ src/Authentications/firebase/firebase.ts
@@ -1,21 +1,27 @@
 import { initializeApp } from "firebase/app";
 import {
-  getAuth,
-  onAuthStateChanged,
-  signOut,
-  signInWithEmailAndPassword,
   NextOrObserver,
   User,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
-import { getFirebaseConfig } from "./fire-config";
+import { config as Fireconfig } from "./fire-config";
 
-const app = initializeApp(getFirebaseConfig());
+const app = initializeApp(Fireconfig);
 const auth = getAuth(app);
 
 export const signInUser = async (email: string, password: string) => {
   if (!email && !password) return;
 
   return await signInWithEmailAndPassword(auth, email, password);
+};
+export const CreateUser = async (email: string, password: string) => {
+  if (!email && !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
 };
 
 export const userStateListener = (callback: NextOrObserver<User>) => {
